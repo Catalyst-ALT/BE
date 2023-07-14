1fe5c0960f39b4f2101a1f79ccbc77d8e9abeef6
Laura-D-Andrews <ldandrews88@gmail.com>
2023-07-14 13:58:44 -0400

    deploy

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'catalyst',
     'rest_framework',
