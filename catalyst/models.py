9072fe032bc35187c1dfb9ede93aa3a8c5c45759
Laura-D-Andrews <ldandrews88@gmail.com>
2023-07-26 13:11:35 -0400

    temporary db too much data fix

--- catalyst/models.py
+++ catalyst/models.py
@@ -65,7 +65,7 @@ class Write(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     note = models.TextField(default='')
     save_prompt = models.BooleanField(default=False)
-    is_deleted = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
     # file = models.FileField(blank=True,
     #                         upload_to='uploads/', validators=[FileTypeValidator(allowed_types=['image/jpeg', 'image/png', 'video/mp4'])])
 
@@ -211,7 +211,7 @@ class Movement(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     note = models.TextField(default='')
     save_prompt = models.BooleanField(default=False)
-    is_deleted = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
     # file = models.FileField(blank=True,
     #                         upload_to='uploads/', validators=[FileTypeValidator(allowed_types=['image/jpeg', 'image/png', 'video/mp4'])])
 
@@ -265,7 +265,7 @@ class Music(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     note = models.TextField(default='')
     save_prompt = models.BooleanField(default=False)
-    is_deleted = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
     # file = models.FileField(blank=True,
     #                         upload_to='uploads/', validators=[FileTypeValidator(allowed_types=['image/jpeg', 'image/png', 'video/mp4'])])
 
